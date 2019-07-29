265ddd05eb5828030f710f3d0708ed7e4b7268f9
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2019-07-29 21:05:18 +0800

    add git for this package.

--- R/dev_history.R
+++ R/dev_history.R
@@ -1,3 +1,15 @@
 # use_build_ignore("R/dev_history.R")
 install()
 library(add2gh)
+
+
+# git ---------------------------------------------------------------------
+
+
+system("git pull origin master")
+backup <- readr::read_lines("../imp_rmd/backup.txt")
+system(glue::glue("git pull {backup} master"))
+
+system("git push origin master")
+backup <- readr::read_lines("../imp_rmd/backup.txt")
+system(glue::glue("git push {backup} master"))
